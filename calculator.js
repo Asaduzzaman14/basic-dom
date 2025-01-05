7190bbce95da056b5e9cdffa82453a874f17e74f
Asaduzzaman14 <asadasad9282@gmail.com>
2025-01-05 17:23:08 +0600

    update

--- calculator.js
+++ calculator.js
@@ -1,22 +1,81 @@
 const input = document.getElementById("input");
+let currentNumber = "";
+let previousNumber = "";
+let operator = "";
 
-let number;
-let operator;
-let result;
-
+// Add event listener to the buttons container
 document.getElementById("buttons").addEventListener("click", (event) => {
   const data = event.target.innerText;
-  console.log(data);
 
-  if (data >= 0 && data < 10) {
-    number = data;
-    input.value = input.value + data;
-  } else if (isNaN(data) && data != "=") {
-    operator = data;
-    input.value = input.value + data;
-  } else if (data == "=") {
-    let res = eval(input.value);
-    console.log(res);
-    input.value = res;
+  if (!isNaN(data) || data === ".") {
+    handleNumber(data);
+  } else if (data === "C") {
+    clearInput();
+  } else if (data === "=") {
+    calculateResult();
+  } else {
+    handleOperator(data);
   }
 });
+
+function handleNumber(num) {
+  if (num === "." && currentNumber.includes(".")) {
+    return;
+  }
+  currentNumber += num;
+  updateInputDisplay(currentNumber);
+}
+
+function handleOperator(op) {
+  if (currentNumber === "") return;
+
+  if (previousNumber && currentNumber && operator) {
+    calculateResult();
+  }
+
+  operator = op;
+  previousNumber = currentNumber;
+  currentNumber = "";
+  updateInputDisplay(previousNumber + " " + operator);
+}
+
+function calculateResult() {
+  if (!previousNumber || !currentNumber || !operator) return;
+
+  const num1 = parseFloat(previousNumber);
+  const num2 = parseFloat(currentNumber);
+  let result;
+
+  switch (operator) {
+    case "+":
+      result = num1 + num2;
+      break;
+    case "-":
+      result = num1 - num2;
+      break;
+    case "*":
+      result = num1 * num2;
+      break;
+    case "/":
+      result = num2 !== 0 ? num1 / num2 : "Error";
+      break;
+    default:
+      return;
+  }
+
+  updateInputDisplay(result);
+  currentNumber = result.toString();
+  previousNumber = "";
+  operator = "";
+}
+
+function clearInput() {
+  currentNumber = "";
+  previousNumber = "";
+  operator = "";
+  updateInputDisplay("");
+}
+
+function updateInputDisplay(value) {
+  input.value = value;
+}
